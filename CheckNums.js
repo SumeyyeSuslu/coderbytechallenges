fc57352a052f6abea35d2580b4adc76db23be3e8
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-07-21 21:32:06 -0500

    Create CheckNums.js

---
+++ CheckNums.js
@@ -0,0 +1,14 @@
+function CheckNums(num1,num2) { 
+   if (num1<num2)
+       return "true";
+      else if (num1==num2)
+          return "-1";
+
+      else 
+          return "false";
+   
+         
+}
+   
+// keep this function call here 
+CheckNums(readline());
